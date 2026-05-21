ff85bcecdb06ab344f04c2b9b69cf5490a3c2054
kakeru_konishi <kakeru.konishi@jrcs.co.jp>
2026-05-21 14:54:38 +0900

    -Did component header content.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -41,6 +41,7 @@ import { ConfirmUpdateDialogComponent } from './components/userMyPage/confirm-up
 import { UserCardHistoryPageComponent } from './components/userCardHistoryPage/userCardHistoryPage.component';
 import { UserLoginDialogComponent } from './components/userLoginDialog/userLoginDialog.component';
 import { AppLogoComponent } from './components/common/appLogo/appLogo.component';
+import { AppHeaderComponent } from './components/common/appHeader/appHeader.component';
 import { GachaBoxComponent } from './components/gachaBox/gachaBox.component';
 import { GachaDrawResultDialogComponent } from './components/gachaDrawResultDialog/gachaDrawResultDialog.component';
 import { CoinExchangeDialogComponent } from './components/coinExchangeDialog/coinExchangeDialog.component';
@@ -70,6 +71,7 @@ import { CoinExchangeDialogComponent } from './components/coinExchangeDialog/coi
     UserCardHistoryPageComponent,
     UserLoginDialogComponent,
     AppLogoComponent,
+    AppHeaderComponent,
     GachaBoxComponent,
     GachaDrawResultDialogComponent,
     CoinExchangeDialogComponent,
